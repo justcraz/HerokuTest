401d2b237c02f1f5b8c2ba9c103437665c3790f0
Alexandr Grigoraschuk <64975380+justcraz@users.noreply.github.com>
2021-06-05 11:44:37 +0300

    my first commit

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @RequestMapping("/hello")
+    String sayhello(){
+        return "<h2>hello from controller<>=/h2";
+    }
+}
